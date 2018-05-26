87c5c684b5dd272732f236c358ed7584ec0a51b6
Daniel Zapata <danielzapata1019@gmail.com>
2018-05-25 22:28:28 -0500

    Inicio Routes Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SliderComponent } from './home/slider/slider.component';
+import { RouteRoutingModule } from './route/route-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SliderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouteRoutingModule,HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
